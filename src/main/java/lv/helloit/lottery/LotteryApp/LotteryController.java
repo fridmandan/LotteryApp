f1fb6a665cdb9394c04c60a8720ebdc3ec4960f6
Dan Fridman <dan.fridman@yahoo.com>
2020-02-04 18:47:45 +0200

    Initial commit

---
+++ src/main/java/lv/helloit/lottery/LotteryApp/LotteryController.java
@@ -0,0 +1,34 @@
+package lv.helloit.lottery.LotteryApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LotteryController {
+
+    @PostMapping("/start-registration")
+    public void startRegistration () {
+
+    }
+
+    @PostMapping("/register")
+    public void registerUser () {
+
+    }
+
+    @PostMapping("/stop-registration")
+    public void stopRegistration () {
+
+    }
+
+    @PostMapping("/choose-winner")
+    public void chooseWinner() {
+
+    }
+
+    @GetMapping("/status")
+    public void getStatus () {
+
+    }
+}
---
+++ src/test/java/lv/helloit/lottery/LotteryApp/LotteryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.helloit.lottery.LotteryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
